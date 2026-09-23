5e42ef3d3b01c3f5bbd4a071356833a22a7d87c7
ruasva7 <Andrey.SEMENOV@raiffeisen.ru>
2026-09-23 13:44:31 +0300

    module 5

--- src/main/java/microarch/delivery/core/domain/services/OrderServiceImpl.java
+++ src/main/java/microarch/delivery/core/domain/services/OrderServiceImpl.java
@@ -1,7 +1,7 @@
 package microarch.delivery.core.domain.services;
 
+import java.util.Comparator;
 import java.util.List;
-import java.util.Optional;
 import libs.errs.Error;
 import libs.errs.Result;
 import libs.errs.UnitResult;
@@ -14,22 +14,19 @@ import org.springframework.stereotype.Service;
 public class OrderServiceImpl implements OrderService {
 
     @Override
-    public Result<Courier, Error> assignOrder(Order order,
-                                              List<Courier> couriers) {
+    public Result<Courier, Error> assignOrder(Order order, List<Courier> couriers) {
         if (!order.isCreated()) {
             return Result.failure(Errors.cantAssignIfOrderStatusIsNotCreated());
         }
-        List<Courier> availableCouriers = couriers.stream().filter(courier -> courier.canTakeOrder(order.getVolume()))
-            .toList();
+        Location orderLocation = order.getLocation();
+        List<Courier> availableCouriers = couriers.stream().filter(courier -> courier.canTakeOrder(order.getVolume())).toList();
         if (availableCouriers.isEmpty()) {
             return Result.failure(Errors.allCouriersAreFullyBookedOrUnavailable());
         }
-        Location orderLocation = order.getLocation();
-        Courier foundCourier = availableCouriers.stream().min((c1, c2) ->
-            Math.min(c1.getLocation().distance(orderLocation),
-                c2.getLocation().distance(orderLocation))).get();
 
-        UnitResult<Error> takingOrderResult = foundCourier.takeOrder(order.getId(), orderLocation, order.getVolume());
+        Courier closestCourier = availableCouriers.stream().min(Comparator.comparingInt(courier -> courier.getLocation().distance(orderLocation))).orElseThrow();
+
+        UnitResult<Error> takingOrderResult = closestCourier.takeOrder(order.getId(), orderLocation, order.getVolume());
         if (takingOrderResult.isFailure()) {
             return Result.failure(takingOrderResult.getError());
         }
@@ -37,16 +34,16 @@ public class OrderServiceImpl implements OrderService {
         if (orderAssignResult.isFailure()) {
             return Result.failure(orderAssignResult.getError());
         }
-        return Result.success(foundCourier);
+        return Result.success(closestCourier);
     }
 
     public static class Errors {
         public static Error cantAssignIfOrderStatusIsNotCreated() {
-            return Error.of( "cant.assign.if.order.status.is.not.created", "The order must be in CREATED status");
+            return Error.of("cant.assign.if.order.status.is.not.created", "The order must be in CREATED status");
         }
+
         public static Error allCouriersAreFullyBookedOrUnavailable() {
-            return Error.of("all.couriers.are.fully.booked.or.unavailable",
-                "All couriers are fully booked or unavailable");
+            return Error.of("all.couriers.are.fully.booked.or.unavailable", "All couriers are fully booked or unavailable");
         }
     }
 }
